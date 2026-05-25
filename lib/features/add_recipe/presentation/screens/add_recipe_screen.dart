84f8842a4c2483316e37cf7476d1be2c8e2790b6
NRM <roman.nikitchuk123@gmail.com>
2026-05-25 20:59:55 +0200

    chore: remove IDE and build folders from tracking

---
+++ lib/features/add_recipe/presentation/screens/add_recipe_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AddRecipeScreen extends StatelessWidget {
+  const AddRecipeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Add Recipe')),
+    );
+  }
+}
